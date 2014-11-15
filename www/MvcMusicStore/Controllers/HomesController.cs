2a50074fbbbc3911b1ad1b6f04d449b33b03a013
zulldesign <admin@zulldesign.ml>
2014-11-15 22:19:41 +0800

    l

---
+++ www/MvcMusicStore/Controllers/HomesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers
+{
+    public class HomesController : Controller
+    {
+        //
+        // GET: /Homes/
+
+        db210beb3bff0344fb8bf4a3a800dda376Entities storeDB = new db210beb3bff0344fb8bf4a3a800dda376Entities();
+
+        public ActionResult Index()
+        {
+            // Get most popular albums
+            var albums = GetTopSellingAlbums(5);
+
+            return View(albums);
+        }
+
+        private List<Album> GetTopSellingAlbums(int count)
+        {
+            // Group the order details by album and return
+            // the albums with the highest count
+
+            return storeDB.Albums
+                .OrderByDescending(a => a.OrderDetails.Count())
+                .Take(count)
+                .ToList();
+        }
+    }
+}
