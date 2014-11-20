49440f1d86b56e2ab1ec8be6b4bc6c7ab2caba31
zulldesign <admin@zulldesign.ml>
2014-11-20 10:21:41 +0800

    l

---
+++ www/MvcMusicStore/Controllers/OfficeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class OfficeController : Controller
+    {
+        //
+        // GET: /Office/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
