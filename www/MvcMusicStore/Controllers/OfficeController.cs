8d6acd65746276c86698915aa3be19ff5be38853
zulldesign <admin@zulldesign.ml>
2014-11-20 11:09:36 +0800

    l

--- www/MvcMusicStore/Controllers/OfficeController.cs
+++ www/MvcMusicStore/Controllers/OfficeController.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Threading.Tasks;
 using System.Web.Mvc;
-using Facebook;
-using Facebook.Web;
-using Facebook.Web.Mvc;
 using Microsoft.AspNet.Mvc.Facebook;
 using Microsoft.AspNet.Mvc.Facebook.Client;
+using MvcMusicStore.Models;
 
 namespace MvcMusicStore.Controllers
 {
